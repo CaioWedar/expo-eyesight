0090597a843e60ebea07b3b30f1f8f71e95e16c2
Caio Fagundes <caiofagundes1@hotmail.com>
2022-11-22 01:45:52 -0300

    feat: new classes i18n and better speak logic

--- src/screens/Camera/CameraScreen.tsx
+++ src/screens/Camera/CameraScreen.tsx
@@ -1,14 +1,16 @@
-import { useFocusEffect } from '@react-navigation/native';
-import React, { useState } from 'react';
-import { StatusBar } from 'react-native';
-import { useModel } from '../../hooks/useModel';
-import { Tensor3D } from '@tensorflow/tfjs-core';
-import * as ObjectDetectionService from '../../services/objectDetection';
+import { useFocusEffect } from "@react-navigation/native";
+import React, { useState } from "react";
+import { StatusBar } from "react-native";
+import { useModel } from "../../hooks/useModel";
+import { Tensor3D } from "@tensorflow/tfjs-core";
+import * as ObjectDetectionService from "../../services/objectDetection";
+import { useTranslation } from "react-i18next";
 
-import * as S from './CameraScreen.styles';
+import * as S from "./CameraScreen.styles";
 
 const CameraScreen = () => {
   const model = useModel();
+  const { t } = useTranslation();
 
   const [isCameraActive, setIsCameraActive] = useState(false);
 
@@ -24,7 +26,7 @@ const CameraScreen = () => {
 
   const runCoco = async (images: IterableIterator<Tensor3D>) => {
     try {
-      await ObjectDetectionService.detectInSnapshot(images, model);
+      await ObjectDetectionService.detectInSnapshot(images, t, model);
     } catch (err) {
       if (err instanceof Error) {
         console.error(err);
--- src/services/objectDetection.tsx
+++ src/services/objectDetection.tsx
@@ -1,7 +1,8 @@
-import { ObjectDetection } from '@tensorflow-models/coco-ssd';
-import { Tensor3D } from '@tensorflow/tfjs';
-import { Platform } from 'react-native';
-import * as SpeechService from '../services/speech';
+import { ObjectDetection } from "@tensorflow-models/coco-ssd";
+import { Tensor3D } from "@tensorflow/tfjs";
+
+import { Platform } from "react-native";
+import * as SpeechService from "../services/speech";
 
 export type ParsedDetection = {
   class: string;
@@ -10,15 +11,16 @@ export type ParsedDetection = {
 
 export const detectInSnapshot = async (
   images: IterableIterator<Tensor3D>,
+  t: (value: string) => string,
   model?: ObjectDetection
 ) => {
   if (!model) {
-    throw new Error('No model');
+    throw new Error("No model");
   }
   const nextImageTensor = images.next().value;
 
   if (!nextImageTensor) {
-    throw new Error('No image tensor');
+    throw new Error("No image tensor");
   }
 
   const predictions = await model.detect(nextImageTensor);
@@ -43,17 +45,19 @@ export const detectInSnapshot = async (
 
   parsedPredictions.forEach((parsedPrediction) => {
     SpeechService.speak(
-      `${parsedPrediction.count > 1 ? parsedPrediction.count : ''} ${
-        parsedPrediction.class
-      }`
+      `${parsedPrediction.count > 1 ? parsedPrediction.count : ""} ${t(
+        parsedPrediction.count > 1
+          ? `classes.plural.${parsedPrediction.class}`
+          : `classes.singular.${parsedPrediction.class}`
+      )}`
     );
   });
 
-  setTimeout(() => detectInSnapshot(images, model), 5000);
+  setTimeout(() => detectInSnapshot(images, t, model), 5000);
 };
 
 export const textureDims =
-  Platform.OS === 'ios'
+  Platform.OS === "ios"
     ? {
         height: 1920,
         width: 1080,
